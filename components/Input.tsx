60cdcfedfaa167a257fdf7dc89a10877a85369d3
Mohamed Achaq <motriyanox@gmail.com>
2022-05-09 00:15:24 +0100

    Input update

--- components/Input.tsx
+++ components/Input.tsx
@@ -21,7 +21,7 @@ const Input = (props: Props) => {
       title={props.title}
       value={props.value}
       onChange={props.onChange}
-      className="w-full rounded-lg bg-gray-100 px-8 py-5 text-lg font-medium text-black placeholder-gray-500 ring-gray-500 ring-offset-4 ring-offset-white transition duration-300 focus-within:outline-none focus-within:ring-2 focus-within:ring-gray-500 hover:ring-2 hover:ring-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-500 disabled:text-gray-400 disabled:ring-0 group-hover:ring-2 group-hover:ring-gray-500 group-focus:ring-2 group-focus:ring-gray-800 dark:bg-zinc-800 dark:text-white dark:ring-offset-gray-900 dark:disabled:text-gray-500 md:w-2/3 lg:w-1/2"
+      className="w-full rounded-lg bg-gray-50 px-8 py-5 text-lg font-medium text-black placeholder-gray-500 ring-gray-500 ring-offset-4 ring-offset-white transition duration-300 focus-within:outline-none focus-within:ring-2 focus-within:ring-gray-500 hover:ring-2 hover:ring-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-500 disabled:text-gray-400 disabled:ring-0 group-hover:ring-2 group-hover:ring-gray-500 group-focus:ring-2 group-focus:ring-gray-800 dark:bg-zinc-900 dark:text-white dark:ring-offset-gray-900 dark:disabled:text-gray-500 md:w-2/3 lg:w-1/2"
       placeholder={props.placeholder}
       required
     />
@@ -40,7 +40,7 @@ export const ContentInput = (props: Props) => {
       pattern={props.pattern}
       value={props.value}
       onChange={props.onChange}
-      className="h-48 w-full rounded-lg bg-gray-100 px-8 py-5 text-lg font-medium text-black placeholder-gray-500 ring-gray-500 ring-offset-4 ring-offset-white transition duration-300 focus-within:outline-none focus-within:ring-2 focus-within:ring-gray-500 hover:ring-2 hover:ring-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-500 disabled:text-gray-400 disabled:ring-0 group-hover:ring-2 group-hover:ring-gray-500 group-focus:ring-2 group-focus:ring-gray-800 dark:bg-zinc-800 dark:text-white dark:ring-offset-gray-900 dark:disabled:text-gray-500 md:w-2/3 lg:w-1/2"
+      className="h-48 w-full rounded-lg bg-gray-50 px-8 py-5 text-lg font-medium text-black placeholder-gray-500 ring-gray-500 ring-offset-4 ring-offset-white transition duration-300 focus-within:outline-none focus-within:ring-2 focus-within:ring-gray-500 hover:ring-2 hover:ring-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-500 disabled:text-gray-400 disabled:ring-0 group-hover:ring-2 group-hover:ring-gray-500 group-focus:ring-2 group-focus:ring-gray-800 dark:bg-zinc-900 dark:text-white dark:ring-offset-gray-900 dark:disabled:text-gray-500 md:w-2/3 lg:w-1/2"
       placeholder={props.placeholder}
       required
     />
@@ -57,7 +57,7 @@ type SelectProps = {
 export const SelectCategory = (props: SelectProps) => {
   return (
     <select
-      className="w-full rounded-lg bg-gray-100 px-8 py-5 text-lg font-medium text-black placeholder-gray-500 ring-gray-500 ring-offset-4 ring-offset-white transition duration-300 focus-within:outline-none focus-within:ring-2 focus-within:ring-gray-500 hover:ring-2 hover:ring-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-500 disabled:text-gray-400 disabled:ring-0 group-hover:ring-2 group-hover:ring-gray-500 group-focus:ring-2 group-focus:ring-gray-800 dark:bg-zinc-800 dark:text-white dark:ring-offset-gray-900 dark:disabled:text-gray-500 md:w-2/3 lg:w-1/2"
+      className="w-full rounded-lg bg-gray-50 px-8 py-5 text-lg font-medium text-black placeholder-gray-500 ring-gray-500 ring-offset-4 ring-offset-white transition duration-300 focus-within:outline-none focus-within:ring-2 focus-within:ring-gray-500 hover:ring-2 hover:ring-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-500 disabled:text-gray-400 disabled:ring-0 group-hover:ring-2 group-hover:ring-gray-500 group-focus:ring-2 group-focus:ring-gray-800 dark:bg-zinc-900 dark:text-white dark:ring-offset-gray-900 dark:disabled:text-gray-500 md:w-2/3 lg:w-1/2"
       required
       name={props.name}
       id={props.id}
