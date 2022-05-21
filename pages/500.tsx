29c5c7b9be032a7a816ce26b01b40620751cf95f
Mohamed Achaq <motriyanox@gmail.com>
2022-05-21 22:40:49 +0100

    New Descript

--- pages/500.tsx
+++ pages/500.tsx
@@ -3,7 +3,7 @@ import Layout from '../layout/Layout'
 import NothingToShow from '../components/NothingToShow'
 const Custom500: NextPage = () => {
   return (
-    <Layout title="500" description="500 page">
+    <Layout title="500" description="Memory sharing platform">
       <NothingToShow />
     </Layout>
   )
