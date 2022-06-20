4de64fd17a795334a16ac4fa418785d9d96d1645
Mohamed Achaq <motriyanox@gmail.com>
2022-06-20 03:21:00 +0100

    profile redesign

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -20,7 +20,7 @@ const Profile = () => {
     <Fragment>
       <ProfileHeader />
       {data && (
-        <div className="mb-4 grid grid-cols-1 items-center justify-center gap-6 px-8 md:px-24 lg:grid-cols-2 xl:grid-cols-3 xl:px-20">
+        <div className="grid grid-cols-1 items-center justify-center gap-6 px-8 pb-16 md:px-24 lg:grid-cols-2 xl:grid-cols-3 xl:px-20">
           {data.map(
             (memory: {
               id: React.Key | null | undefined
