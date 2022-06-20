f3321c96585bff4265d77fa840caeff01f82f37a
Mohamed Achaq <motriyanox@gmail.com>
2022-06-20 03:23:37 +0100

    adding padding bottom

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,11 +7,11 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;200;300;400;500;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
       </Head>
-      <body className="bg-white antialiased dark:bg-black">
+      <body className="bg-white pb-8 antialiased dark:bg-black">
         <Main />
         <NextScript />
       </body>
