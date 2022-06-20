613a0c87290297cd197e2be0943dc8c1d28bc964
Mohamed Achaq <motriyanox@gmail.com>
2022-06-20 03:18:43 +0100

    New buttons styles

--- components/Buttons.tsx
+++ components/Buttons.tsx
@@ -1,93 +1,88 @@
 import { ReactNode } from 'react'
-type Props = {
+
+type FilledButton = {
   text: string
   onClick?: () => void
 }
 
-export const Button = (props: Props) => {
-  return (
-    <button className="flex w-full flex-row items-center justify-center gap-2 rounded-xl bg-black px-6 py-2 text-xl text-white transition-all duration-300 hover:scale-105 hover:shadow-[0_35px_60px_-15px] active:scale-95 dark:bg-white dark:text-black md:w-auto">
-      {props.text}{' '}
-    </button>
-  )
-}
-
-export const ButtonWhite = (props: Props) => {
-  return (
-    <button className="flex flex-row items-center justify-center gap-2 rounded-xl px-6 py-2 text-xl text-black transition-all duration-200 hover:scale-105 active:scale-95 dark:text-white">
-      {props.text}{' '}
-    </button>
-  )
-}
-
-export const ShareButton = (props: { elements: ReactNode[] }) => {
-  return (
-    <button className="flex flex-row items-center justify-center gap-2 rounded-xl bg-black px-6 py-2 text-xl text-white transition-all duration-300 hover:scale-105 hover:shadow-[0_35px_60px_-15px] active:scale-95 dark:bg-white dark:text-black">
-      {props.elements}
-    </button>
-  )
-}
-
-export const SubmitButton = (props: Props) => {
+export const FilledButton = (props: FilledButton) => {
   return (
     <button
-      type="submit"
-      className="flex w-full flex-row items-center justify-center gap-2 rounded-xl bg-black px-20 py-3 text-xl text-white transition-all duration-300 hover:scale-105 hover:shadow-[0_35px_60px_-15px] active:scale-95 dark:bg-white dark:text-black md:w-2/3 lg:w-1/2"
+      onClick={props.onClick}
+      className="rounded-full bg-black px-6 py-2 text-xl font-bold text-white ring-zinc-200 transition-all duration-300 hover:scale-105 hover:ring-4 active:scale-95 dark:bg-white dark:text-black dark:ring-zinc-800"
     >
-      {props.text}{' '}
+      {props.text}
     </button>
   )
 }
 
-export const DeleteButton = (props: Props) => {
+export const ActionButton = (props: FilledButton) => {
+  return (
+    <button className="rounded-full bg-black px-8 py-3 text-lg font-bold text-white ring-0 ring-zinc-200 transition-all duration-300 hover:scale-105 hover:ring-4 active:scale-95 dark:bg-white dark:text-black dark:ring-zinc-800">
+      {props.text}
+    </button>
+  )
+}
+
+type IconButton = {
+  icon: ReactNode
+  onclick?: () => void
+}
+
+export const IconButton = (props: IconButton) => {
+  return (
+    <button
+      onClick={props.onclick}
+      className="rounded-full bg-black p-2 text-xl font-bold text-white ring-zinc-200 transition-all duration-300 hover:scale-105 hover:ring-4 active:scale-95 dark:bg-white dark:text-black dark:ring-zinc-800"
+    >
+      {props.icon}
+    </button>
+  )
+}
+
+type SubmitButton = {
+  text: string
+  sm?: boolean
+  onClick?: () => void
+}
+
+export const SubmitButton = (props: SubmitButton) => {
   return (
     <button
       type="submit"
       onClick={props.onClick}
-      className="flex w-full flex-row items-center justify-center gap-2 rounded-xl bg-red-600 px-20 py-3 text-xl text-white transition-all duration-300 hover:scale-105 hover:shadow-[0_35px_60px_-15px] active:scale-95 dark:bg-red-700 dark:text-black md:w-2/3 lg:w-1/2"
+      className={`${
+        props.sm ? 'w-full sm:w-[360px]' : 'w-[360px]'
+      } mt-4 rounded-2xl bg-zinc-900 py-3 px-4 font-bold text-white transition-all duration-300 hover:ring-4 hover:ring-zinc-300 active:scale-95 dark:bg-zinc-100 dark:text-black dark:hover:ring-zinc-700 md:w-[400px]`}
     >
-      {props.text}{' '}
+      {props.text}
     </button>
   )
 }
 
-export const LoginDemo = (props: Props) => {
+export const BorderButton = (props: SubmitButton) => {
   return (
     <button
-      type="submit"
       onClick={props.onClick}
-      className="flex w-full flex-row items-center justify-center gap-2 rounded-xl border-2 border-zinc-800 px-20 py-3 text-xl text-zinc-800 transition-all duration-300 hover:scale-105 active:scale-95 dark:border-zinc-200 dark:text-zinc-200 md:w-2/3 lg:w-1/2"
+      className={`${
+        props.sm ? 'w-full sm:w-[360px]' : 'w-[360px]'
+      } mt-4 rounded-2xl border-2 border-zinc-900 py-3 px-4 font-bold text-black transition-all duration-300 hover:ring-4 hover:ring-zinc-300 active:scale-95 dark:border-zinc-100 dark:text-white dark:hover:ring-zinc-700 md:w-[400px]`}
     >
-      {props.text}{' '}
+      {props.text}
     </button>
   )
 }
 
-type PropsDel = {
-  children: ReactNode
-  onClick: (e: Event | any) => void
-}
-
-export const DeleteButtonWithIcon = (props: PropsDel) => {
+export const DeleteButton = (props: SubmitButton) => {
   return (
     <button
       type="submit"
       onClick={props.onClick}
-      className="flex flex-row items-center justify-center gap-2 rounded-xl px-4 py-2 text-xl text-red-700 active:scale-95 dark:text-red-600"
+      className={`${
+        props.sm ? 'w-full sm:w-[360px]' : 'w-[360px]'
+      } mt-4 rounded-2xl bg-red-500 py-3 px-4 font-bold text-white transition-all duration-300 hover:ring-4 hover:ring-red-300 active:scale-95 dark:bg-red-600 dark:text-black dark:hover:ring-red-400 md:w-[400px]`}
     >
-      {props.children}
-    </button>
-  )
-}
-
-export const EditButtonWithIcon = (props: PropsDel) => {
-  return (
-    <button
-      type="submit"
-      onClick={props.onClick}
-      className="flex flex-row items-center justify-center gap-2 rounded-xl px-4 py-2 text-xl text-zinc-800 active:scale-95 dark:text-zinc-200"
-    >
-      {props.children}
+      {props.text}
     </button>
   )
 }
